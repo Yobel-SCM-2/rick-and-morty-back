24a5c652bd0d9e5e4ed3fbff74e1eccf55a1a2c6
TadeoPG <tadeomanuelportillo@gmail.com>
2025-04-15 18:09:40 -0500

    :fire: first commit

---
+++ src/main/java/com/yobel/rickandmortyback/RickAndMortyBackApplication.java
@@ -0,0 +1,13 @@
+package com.yobel.rickandmortyback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickAndMortyBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RickAndMortyBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yobel/rickandmortyback/RickAndMortyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yobel.rickandmortyback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickAndMortyBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
